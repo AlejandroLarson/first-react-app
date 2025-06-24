fba1f4d5b359fd25b2988b490f5b5d4a967d637a
alejandrolarson <alejandrolarson1@yahoo.com>
2025-06-24 15:49:46 -0700

    Step 1 card looks fine

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import './App.css'
+
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 // import characters from './protagonists.json'
 
 function App() {
   return (
     <>
+      
       <Container maxWidth="md" sx={{ mb: 4 }}>
         <Typography
           variant="h2"
@@ -27,16 +34,27 @@ function App() {
       </Container>
 
       <Container maxWidth="lg">
-        <Button
-          variant="contained"
-          // I'm trying to use custom CSS defined in the file App.css,
-          // but it isn't working. Why, and how can I fix it?
-          sx = {{
-            backgroundColor: "red"
-          }}
-        > 
-          Red Button?
-        </Button>
+<Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
       </Container>
     </>
   )
